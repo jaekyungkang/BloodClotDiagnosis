9eee6e238d9e3d5bfaf353576d5ee972db01cbae
wzhu03 <108113366+wzhu03@users.noreply.github.com>
2024-04-21 04:39:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask,jsonify
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import Flask, jsonify
 from gemniapi import gen_res
+from flask_cors import CORS
+
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'dcm', 'webp'}
 
 app = Flask(__name__)
+CORS(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route("/")
-def hello_world():
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return jsonify({"message": "foile"})
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
     final_calc = gen_res(0.8)
     return jsonify({"message": final_calc.text})
